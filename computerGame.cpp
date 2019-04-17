707b07519094f97b4b74b09d3390baad88b4e1c8
sailfish009 <sailfish009@gmail.com>
2019-04-17 12:56:31 +0900

    computerGame : sort vector

--- computerGame.cpp
+++ computerGame.cpp
@@ -24,7 +24,6 @@
 //
 //========================================================================
 
-
 #include <string>
 #include <vector>
 #include <iomanip>
@@ -35,6 +34,9 @@
 #include <assert.h>
 #include <cstdint>
 #include <numeric>
+#include <chrono>
+#include <random>
+
 
 // https://stackoverflow.com/questions/10956543/gcd-function-in-c-sans-cmath-library
 template <typename Number>
@@ -51,55 +53,43 @@ Number GCD(Number u, Number v)
 
 int computerGame(std::vector<int> a, std::vector<int> b)
 {
+#if false
+  std::chrono::high_resolution_clock::time_point t1 = 
+    std::chrono::high_resolution_clock::now();
+#endif
+
   size_t a_size = a.size();
   size_t b_size = b.size();
   int pair_count = 0;
   int count = 0;
 
-  std::vector<int> a_index;
-  std::vector<int> b_index;
+  // array must to be sorted
+  std::sort(a.begin(), a.end(), std::less<int>());
+  std::sort(b.begin(), b.end(), std::less<int>());
 
-  for(size_t i =0; i<a_size; ++i)
-  for(size_t j =0; j<b_size; ++j)
+  size_t prev_a = a_size;
+  size_t prev_b = b_size;
+
+  for (size_t i = 0; i < a_size; ++i)
+  for (size_t j = 0; j < b_size; ++j)
   {
-    size_t a_index_size = a_index.size();
-    size_t b_index_size = b_index.size();
-
-    if (a_index_size != 0 && b_index_size != 0)
+    if ( i != prev_a && j != prev_b && GCD(a[i], b[j]) != 1)
     {
-      bool b_coprime = false;
-      for(size_t k =0; k < a_index_size; ++k)
-      {
-        if (i == a_index[k]) { b_coprime = true; break; }
-      }
-
-      for(size_t l =0; l < b_index_size; ++l)
-      {
-        if (j == b_index[l]) { b_coprime = true; break; }
-      }
-
-      if (b_coprime == false)
-      {
-        if (GCD(a[i], b[j]) != 1)
-        {
-          a_index.push_back(i);
-          b_index.push_back(j);
-          // printf("%d [%d] [%d]\n", ++count, a[i], b[j]);
-          ++pair_count;
-        }
-      }
-    }
-    else
-    {
-      if (GCD(a[i], b[j]) != 1)
-      {
-        a_index.push_back(i);
-        b_index.push_back(j);
-        // printf("%d [%d] [%d]\n", ++count, a[i], b[j]);
-        ++pair_count;
-      }
+      prev_a = i;
+      prev_b = j;
+      // printf("%d [%d] [%d]\n", ++count, a[i], b[j]);
+      ++pair_count;
     }
   }
+
+#if false
+  std::chrono::duration<double> time_span = 
+    std::chrono::duration_cast<std::chrono::duration<double>>
+    (std::chrono::high_resolution_clock::now() - t1);
+
+  printf("%lf\n", time_span.count());
+  printf("-end-\n");
+#endif
   return pair_count;
 }
 
@@ -112,51 +102,93 @@ int function()
     std::istringstream tokenStream(s);
     while (std::getline(tokenStream, token, delimiter))
     {
-       tokens.push_back(token);
+      tokens.push_back(token);
     }
     return tokens;
   };
 
-  // array element A 
-  std::string element_a("");
-  // array element B 
-  std::string element_b("");
+  int pair_count = 0;
 
-  std::getline(std::cin, element_a);
-  std::getline(std::cin, element_b);
+  // array element count 
+  std::string element_c("");
+  std::getline(std::cin, element_c);
+  std::vector<std::string> arg1 = split(element_c, ' ');
+  if (arg1.size() == 1)
+  {
+    int elem_c = std::stoi(arg1[0]);
+    // array element A 
+    std::string element_a("");
+    // array element B 
+    std::string element_b("");
 
-  // printf("[c :%s]\n", element_a.c_str());
-  // printf("[v :%s]\n", element_b.c_str());
+    std::getline(std::cin, element_a);
+    std::getline(std::cin, element_b);
 
-  std::vector<std::string> arg1 = split(element_a, ' ');
-  std::vector<std::string> arg2 = split(element_b, ' ');
+    // printf("[c :%s]\n", element_a.c_str());
+    // printf("[v :%s]\n", element_b.c_str());
 
-  std::vector<int> a;
-  std::vector<int> b;
+    std::vector<std::string> arga = split(element_a, ' ');
+    std::vector<std::string> argb = split(element_b, ' ');
 
-  // string to int in a
-  std::transform
-  (
-    arg1.begin(), arg1.end(), std::back_inserter(a),
-    [](const std::string & str) {return std::stoi(str); }
-  );
+    if (arga.size() == elem_c && argb.size() == elem_c)
+    {
+      std::vector<int> a;
+      std::vector<int> b;
 
-  // string to int in b
-  std::transform
-  (
-    arg2.begin(), arg2.end(), std::back_inserter(b),
-    [](const std::string & str) {return std::stoi(str); }
-  );
+      // string to int in a
+      std::transform
+      (
+        arga.begin(), arga.end(), std::back_inserter(a),
+        [](const std::string & str) {return std::stoi(str); }
+      );
 
-  return computerGame(a, b);
+      // string to int in b
+      std::transform
+      (
+        argb.begin(), argb.end(), std::back_inserter(b),
+        [](const std::string & str) {return std::stoi(str); }
+      );
+
+      pair_count = computerGame(a, b);
+    }
+  }
+
+  return pair_count;
 }
 
-#if false
 int main()
 {
+
+#if false 
+  // const int nrolls = 10000; 
+  // const int dist = 1000000000; 
+
+  const int nrolls = 1000; 
+  const int dist = 1000000000; 
+
+  std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution(2, dist);
+
+  std::vector<int> a(nrolls);
+  std::vector<int> b(nrolls);
+
+  for (int i = 0; i < nrolls; ++i)
+  {
+    int numbera = distribution(generator);
+    int numberb = distribution(generator);
+    a.push_back(numbera);
+    b.push_back(numberb);
+  }
+  int count = computerGame(a, b);
+#endif
+
+#if false 
+  std::vector<int> a; a.push_back(2); a.push_back(5); a.push_back(6); a.push_back(7);
+  std::vector<int> b; b.push_back(4); b.push_back(9); b.push_back(10); b.push_back(12);
+  int count = computerGame(a, b);
+#endif
+
   int count = function();
   printf("%d\n", count);
   return 0;
 }
-#endif
-
